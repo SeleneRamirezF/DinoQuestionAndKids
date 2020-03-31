e8394d7ee218b345ce7d486e163978ef76ea0ddc
SeleneRamirezF <47412590+SeleneRamirezF@users.noreply.github.com>
2020-03-31 21:54:03 +0200

    Crear pantalla de carga

---
+++ app/src/main/java/com/example/dinoquestionandkids/CargaActivity.java
@@ -0,0 +1,24 @@
+package com.example.dinoquestionandkids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class CargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(CargaActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, 4000);
+    }
+}
