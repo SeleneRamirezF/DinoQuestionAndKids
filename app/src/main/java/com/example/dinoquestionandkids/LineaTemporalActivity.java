aae084c6a7f9d8364ddd7fededa1971947552966
SeleneRamirezF <47412590+SeleneRamirezF@users.noreply.github.com>
2020-04-05 23:18:51 +0200

    Introducir las imagenes de los fragments Crear el tabbelactivity

--- app/src/main/java/com/example/dinoquestionandkids/LineaTemporalActivity.java
+++ app/src/main/java/com/example/dinoquestionandkids/LineaTemporalActivity.java
@@ -1,10 +1,14 @@
 package com.example.dinoquestionandkids;
 
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.example.dinoquestionandkids.R;
+import android.view.View;
+import com.example.dinoquestionandkids.ui.main.SectionsPagerAdapter;
 
 public class LineaTemporalActivity extends AppCompatActivity {
 
@@ -12,5 +16,20 @@ public class LineaTemporalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linea_temporal);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
