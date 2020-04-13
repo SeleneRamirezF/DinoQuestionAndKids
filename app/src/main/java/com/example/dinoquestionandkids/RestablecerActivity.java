37d9a5830d3a1dac7d6ddfe9b338a5f46b04fd4e
SeleneRamirezF <47412590+SeleneRamirezF@users.noreply.github.com>
2020-04-13 21:23:38 +0200

    opcion de recuperar contraseña

---
+++ app/src/main/java/com/example/dinoquestionandkids/RestablecerActivity.java
@@ -0,0 +1,74 @@
+package com.example.dinoquestionandkids;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RestablecerActivity extends AppCompatActivity {
+
+    private EditText etCorreo;
+    private Button btnRestablecer;
+    private String correo;
+    private FirebaseAuth miAuth;
+    private ProgressDialog miDialogo;
+
+    public RestablecerActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restablecer);
+
+        miAuth = FirebaseAuth.getInstance();
+        miDialogo = new ProgressDialog(this);
+
+        cargarViews();
+
+        btnRestablecer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correo = etCorreo.getText().toString();
+                if (!correo.isEmpty()){
+                    miDialogo.setMessage("Enviando correo...");
+                    miDialogo.setCanceledOnTouchOutside(false);
+                    miDialogo.show();
+                    restablecerContrasena();
+                }else{
+                    Toast.makeText(RestablecerActivity.this, "Tiene que introducir un correo registrado",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void cargarViews() {
+        etCorreo = (EditText) findViewById(R.id.etCorreo);
+        btnRestablecer = (Button) findViewById(R.id.btnRestablecer);
+    }
+
+    private void restablecerContrasena(){
+        miAuth.setLanguageCode("es");
+        miAuth.sendPasswordResetEmail(correo).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(RestablecerActivity.this, "Se ha enviado un correo para restablecer la contraseña",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RestablecerActivity.this, "No se pudo enviar el correo electrónico para restablecer la contraseña",Toast.LENGTH_SHORT).show();
+                }
+                miDialogo.dismiss();
+            }
+        });
+    }
+}
