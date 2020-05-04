c199e1a0a94c23a0c9829ee304d88f4c5af424be
SeleneRamirezF <47412590+SeleneRamirezF@users.noreply.github.com>
2020-05-04 21:53:46 +0200

    Funcionaliento del puzzle terminado!!!!!!

--- app/src/main/java/com/example/dinoquestionandkids/PuzzleActivity.java
+++ app/src/main/java/com/example/dinoquestionandkids/PuzzleActivity.java
@@ -10,6 +10,12 @@ import android.widget.Button;
 public class PuzzleActivity extends AppCompatActivity {
 
     private Button btnPuzzle1;
+    private Button btnPuzzle2;
+    private Button btnPuzzle3;
+    private Button btnPuzzle4;
+    private Button btnPuzzle5;
+    private Button btnPuzzle6;
+    private int boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +23,70 @@ public class PuzzleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_puzzle);
 
         btnPuzzle1 = findViewById(R.id.btnPuzzle1);
+        btnPuzzle2 = findViewById(R.id.btnPuzzle2);
+        btnPuzzle3 = findViewById(R.id.btnPuzzle3);
+        btnPuzzle4 = findViewById(R.id.btnPuzzle4);
+        btnPuzzle5 = findViewById(R.id.btnPuzzle5);
+        btnPuzzle6 = findViewById(R.id.btnPuzzle6);
+
 
         btnPuzzle1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(PuzzleActivity.this, Puzzle2Activity.class));
+                boton = 1;
+                Intent i = new Intent(PuzzleActivity.this, Puzzle2Activity.class);
+                i.putExtra("boton", boton);
+                startActivity(i);
+            }
+        });
+
+        btnPuzzle2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton = 2;
+                Intent i = new Intent(PuzzleActivity.this, Puzzle2Activity.class);
+                i.putExtra("boton", boton);
+                startActivity(i);
+            }
+        });
+
+        btnPuzzle3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton = 3;
+                Intent i = new Intent(PuzzleActivity.this, Puzzle2Activity.class);
+                i.putExtra("boton", boton);
+                startActivity(i);
+            }
+        });
+
+        btnPuzzle4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton = 4;
+                Intent i = new Intent(PuzzleActivity.this, Puzzle2Activity.class);
+                i.putExtra("boton", boton);
+                startActivity(i);
+            }
+        });
+
+        btnPuzzle5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton = 5;
+                Intent i = new Intent(PuzzleActivity.this, Puzzle2Activity.class);
+                i.putExtra("boton", boton);
+                startActivity(i);
+            }
+        });
+
+        btnPuzzle6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton = 6;
+                Intent i = new Intent(PuzzleActivity.this, Puzzle2Activity.class);
+                i.putExtra("boton", boton);
+                startActivity(i);
             }
         });
 
